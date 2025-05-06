4c255a27dae4a8175b680fbbdc32ab740e3cf101
KapibaraUa <126168313+KapibaraUa@users.noreply.github.com>
2025-05-07 00:19:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication31/ConsoleApplication31.cpp
@@ -0,0 +1,44 @@
+﻿
+
+#include <iostream>
+using namespace std;
+char* mystrchr(char* str, char s) {
+    while (*str) {
+        if (*str == s) {
+            return str;  
+        }
+        else
+        {
+            return 0;
+        }
+        str++;
+    }
+    
+}
+char* mystrstr(char* str1, char* str2) {
+    if (*str2 == '\0') {
+        return str1;  
+    }
+
+    for (; *str1 != '\0'; str1++) {
+        char* p1 = str1;
+        char* p2 = str2;
+
+        while (*p1 && *p2 && (*p1 == *p2)) {
+            p1++;
+            p2++;
+        }
+
+        if (*p2 == '\0') {
+            return str1;  
+        }
+    }
+
+    return 0;  
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
